50c22ccb6448e6764485f3ce5129c7b9d70f6ebb
SeyedMohammadHadi Hoseini <seyedmohadihoseini@gmail.com>
2024-06-24 10:17:11 +0330

    f

--- src/app/api/nowpayment/route.ts
+++ src/app/api/nowpayment/route.ts
@@ -6,6 +6,21 @@ export async function POST(request: NextRequest) {
         return Response.json({});
     }
     const body = await request.text();
-    console.log(body);
+    const response = JSON.parse(body);
+    const order_id = response?.order_id;
+    const status = response?.payment_status;
+    console.log(`orderid:${order_id} and status=${status}`);
     return Response.json({});
 }
+const rt = {
+    "actually_paid": 0,
+    "actually_paid_at_fiat": 0,
+    "fee": { "currency": "btc", "depositFee": 0, "serviceFee": 0, "withdrawalFee": 0 },
+    "invoice_id": 5781891653, "order_description": null, "order_id": "12355",
+    "outcome_amount": 0.0001279, "outcome_currency": "btc", "parent_payment_id": null,
+    "pay_address": "TAE7Qgda22uudMCA85E3iuq94QUPGsJC2a", "pay_amount": 9.957785,
+    "pay_currency": "usdttrc20", "payin_extra_id": null, "payment_extra_ids": null,
+    "payment_id": 5629793529, "payment_status": "waiting",
+    "price_amount": 10, "price_currency": "usd", "purchase_id": "5395052836",
+    "updated_at": 1719211088600
+};
--- src/app/sample/page.tsx
+++ src/app/sample/page.tsx
@@ -1,36 +1,25 @@
-// "use client"
-// import NowPaymentsApi from '@nowpaymentsio/nowpayments-api-js'
-// import { randomUUID } from 'crypto';
-// import { useRouter } from 'next/navigation';
-// import { useEffect, useState } from 'react';
+"use client"
+import NowPaymentsApi from '@nowpaymentsio/nowpayments-api-js'
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
-import { randomUUID } from "crypto";
 
-// const npApi = new NowPaymentsApi({ apiKey: "50ZKY1K-VZDMZZM-GW7SPB0-GSR10GR" });
-// export default function SampleApp() {
-//     const router = useRouter();
-//     const [status, setStatus] = useState("")
-//     useEffect(() => {
-//         setStatus(randomUUID());
-//         async function fetchCurrencies() {
-//             const st = await npApi.createInvoice({ price_amount: 10, price_currency: "usd", order_id: "12355", cancel_url: "www.google.com", success_url: "www.google.com", ipn_callback_url: "https://my.algorixfinance.com/api/nowpayment" });
-//             console.log(st);
-//             // router.push(st.invoice_url);
-//         }
-//         fetchCurrencies()
-//     }, [])
-//     return <div>
-//         <h2>Available currencies</h2>
-//         <br />
-
-//         <p>{status}</p>
-//     </div>
-// }
-export default function Sample(){
-
-    const ran = randomUUID();
-
+const npApi = new NowPaymentsApi({ apiKey: "50ZKY1K-VZDMZZM-GW7SPB0-GSR10GR" });
+export default function SampleApp() {
+    const router = useRouter();
+    const [status, setStatus] = useState("")
+    useEffect(() => {
+        async function fetchCurrencies() {
+            const st = await npApi.createInvoice({ price_amount: 10, price_currency: "usd", order_id: "12355", cancel_url: "www.google.com", success_url: "www.google.com", ipn_callback_url: "https://my.algorixfinance.com/api/nowpayment?ipn=51ff4f21-5f09-44b3-9fef-0e087e98280b" });
+            console.log(st);
+            router.push(st.invoice_url);
+        }
+        fetchCurrencies()
+    }, [])
     return <div>
-        {ran}   
+        <h2>Available currencies</h2>
+        <br />
+
+        <p>{status}</p>
     </div>
 }
